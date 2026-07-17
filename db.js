6bc8f9ecc9e88453462347fbfdb4c5d7f1290daf
Shashank S <shankashines@gmail.com>
2026-07-17 19:07:47 +0530

    sql hosted

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-
 const mysql = require('mysql2');
 require('dotenv').config();
 
 let pool;
-
 
 if (process.env.DATABASE_URL) {
     pool = mysql.createPool({
         uri: process.env.DATABASE_URL,
         waitForConnections: true,
         connectionLimit: 10,
-        queueLimit: 0
+        queueLimit: 0,
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
 } else {
-
     pool = mysql.createPool({
-        host: process.env.DB_HOST || '127.0.0.0.1',
+        host: process.env.DB_HOST || '127.0.0.1',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE || 'quora_db',
