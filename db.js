515ecc87cd9a879ade94d7d38941b79ddb50876b
Shashank S <shankashines@gmail.com>
2026-07-17 18:53:12 +0530

    sql hosted

--- db.js
+++ db.js
@@ -1,15 +1,28 @@
-// db.js
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',       
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_DATABASE || 'quora_db',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let pool;
+
+
+if (process.env.DATABASE_URL) {
+    pool = mysql.createPool({
+        uri: process.env.DATABASE_URL,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+} else {
+
+    pool = mysql.createPool({
+        host: process.env.DB_HOST || '127.0.0.0.1',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE || 'quora_db',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+}
 
 module.exports = pool.promise();
